1f810303f58388efd78cbb20548c01e9e8f7fa9f
arthdantas <arthdantas05@gmail.com>
2024-02-01 09:04:53 -0300

    Criando a classe Posicao

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml;
+using xadrez_console.tabuleiro;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Posicao P = new Posicao(3, 4);
+
+        Console.WriteLine("Posição ="+ P);
+
+    }
+}
