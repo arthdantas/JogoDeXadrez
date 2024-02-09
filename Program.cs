84d9d408da4aba275fb79b5ef905c27f530adab4
arthdantas <arthdantas05@gmail.com>
2024-02-09 15:54:29 -0300

    Controlando as pecas capturadas

--- Program.cs
+++ Program.cs
@@ -19,10 +19,8 @@ class Program
                 try
                 {
                     Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.WriteLine("Turno: " + partida.turno);
-                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                    Tela.imprimirPartida(partida);
+                    
 
 
                     Console.WriteLine();
